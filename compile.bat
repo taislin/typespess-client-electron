8daa36ecc72a5b36a634ef4c8058debee68cf0ce
Taislin <cariosecus@gmail.com>
2021-02-28 01:20:03 +0000

    scripts

--- compile.bat
+++ compile.bat
@@ -1,4 +1,7 @@
+npm install 7zip -g
 git clone https://github.com/civ13/typespess-client client_src
+curl -o resources.zip https://github.com/Civ13/civ13-typespess/raw/master/resources.zip
+7z e resources.zip -oresources -y -r
 cd client_src
 git reset --hard origin/main
 git pull
