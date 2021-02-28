f49bcac24a5d9c73eb4613242e2ec380771bdf87
Taislin <cariosecus@gmail.com>
2021-02-28 01:31:19 +0000

    script fixes

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 npm install 7zip -g
 git clone https://github.com/civ13/typespess-client client_src
 wget -o resources.zip https://github.com/Civ13/civ13-typespess/raw/master/resources.zip
-7z e resources.zip -oresources-y -r
+7z x resources.zip -y
 cd client_src
 git reset --hard origin/main
 git pull
